b77c27006cc9bcd066d2e724d1c554e72e236062
Sancar Adali <sadali1@jhu.edu>
2014-04-28 20:57:01 -0400

    run sgm via IP for worm subgraphs (all gurobi model files in one dir): GraphMatching_Infeasible_bulk.py

--- GraphMatching_Infeasible_bulk.py
+++ GraphMatching_Infeasible_bulk.py
@@ -1,40 +1,131 @@
 __author__ = 'Sancar'
 
 from gurobipy import *
+import re
 import os
+import time
 import copy
-import pymatlab
+
 import numpy as np
+import ggplot as gg
+from scipy import stats as st
+from pandas import DataFrame as pd_df
+
+
+os.environ["LD_LIBRARY_PATH"] = os.environ["LD_LIBRARY_PATH"] + ":/usr/lib/x86_64-linux-gnu/libstdc++.so.6"
+import pymatlab
+
+
 
 pr_dir = os.environ.get('PROJECT_DIR')
-session = pymatlab.session_factory()
+session = pymatlab.session_factory(options=" -logfile sgm_ip_run_log"+time.strftime("%H:%M:%S")+".txt")
 
 #pr_dir = 'G:\Sancar\Documents\projects'
-#pr_dir = '~/projects/'
+pr_dir = '~/projects/'
 print pr_dir
 
-root_dir  = os.path.join(pr_dir ,'SeededGraphMatch')
-models_dir = os.path.join(root_dir ,'optim_models')
+pr_dir =  '/home/sadali1/projects/'
+root_dir  = os.path.join(pr_dir, 'SeededGraphMatch')
+models_dir = os.path.join(root_dir, 'optim_models')
 
 
-for file in os.listdir(models_dir):
-    if file.endswith(".lp"):
-        gmodel = read(fname)
-        gmodel.update()
+chdir_script = """cd """ + root_dir
+#chdir_script = """cd G:\\Sancar\\Documents\\projects\\SeededGraphMatch"""
+session.putvalue('chdir_script', chdir_script)
 
 
-        allConstr = gmodel.getConstrs( )
-        relaxedConstr_22= allConstr[0:(n*n)]
-        relaxedConstr_12= allConstr[(n*n):(n*n+2*m*n)]
-        pen_Constr_22 = [1.0]*len(relaxedConstr_22)
-        pen_Constr_12 = [1.0]*len(relaxedConstr_12)
-        gmodel_relaxed = gmodel.copy()
-        gmodel_relaxed.feasRelax(0, False, None, None, None, relaxedConstr_22+relaxedConstr_12, pen_Constr_22+pen_Constr_12)
-        gmodel_relaxed.update()
-        gmodel_relaxed.write(fname+'relaxed.lp')
-        optresults = gmodel_relaxed.optimize()
+session.run('eval(chdir_script)')
+session.run('cur_dir  = pwd')
+returndir = session.getvalue('cur_dir')
+print returndir
+session.run('addpath(genpath(pwd))')
+session.run("addpath('lib')")
+session.run("addpath('src')")
 
-        v = gmodel.getVars()
-        print v
-        for var in v:
-            print var
+
+truematch_orig = []
+runtime_orig = []
+truematch_mod = []
+runtime_mod = []
+
+subg_sizes = [118, 83, 78]
+
+nmc = 0
+subgraph_id  = []
+
+for fname in os.listdir(models_dir):
+    if fname.endswith(".lp"):
+        try:
+            pathfile = os.path.join(models_dir, fname)
+            print "Processing" + fname
+            gmodel = read(pathfile)
+            gmodel.update()
+            gmodel_relaxed = gmodel.copy()
+            allConstr = gmodel_relaxed.getConstrs()
+            fname_ints = re.findall(r'\d+', fname)
+            subg_index = int(fname_ints[0])
+            subgraph_id.append(subg_index)
+            mplusn = subg_sizes[subg_index-1];
+            m = int(fname_ints[1])
+            n= mplusn -m
+            mc = int(fname_ints[2])
+            relaxedConstr_22= allConstr[0:(n*n)]
+            relaxedConstr_12= allConstr[(n*n):(n*n+2*m*n)]
+            pen_Constr_22 = [1.0]*len(relaxedConstr_22)
+            pen_Constr_12 = [5.0]*len(relaxedConstr_12)
+            gmodel_relaxed.feasRelax(0, False, None, None, None, relaxedConstr_22+relaxedConstr_12, pen_Constr_22+pen_Constr_12)
+            gmodel_relaxed.update()
+            gmodel_relaxed.write(pathfile+'relaxed.lp')
+            optresults = gmodel_relaxed.optimize()
+            print("Relaxed problems solved")
+            v_alt = gmodel_relaxed.getVars()
+            P_alt = np.zeros((n*n, 1))
+            testm = n
+            testm_npy = np.array([testm])
+            session.putvalue('testm', testm_npy)
+            mplusn_npy = np.array([mplusn])
+            session.putvalue('mplusn', mplusn_npy)
+            seed_m_npy = np.array([m])
+            session.putvalue('seedm', seed_m_npy)
+            print ("MATLAB vars created")
+            session.run("true_perm = 1:mplusn")
+            session.run("true_perm= double(true_perm)")
+            session.run("seedm= double(seedm)")
+            print ("MATLAB vars converted to double")
+            it = 0
+            for var_a in v_alt[0:(n*n)]:
+                P_alt[it, 0] = var_a.X
+                it += 1
+            P_alt = P_alt.reshape((n, n)).transpose()
+            session.putvalue('mod_Phat', P_alt)
+            run_script = '[mod_tm_m'+str(mc) +', mod_delta_m'+str(mc) +'] = PermMat2TrueMatch(mod_Phat, true_perm, seedm)'
+            try:
+                session.run(run_script)
+                truematch_mod.append(session.getvalue("mod_delta_m"+str(mc)))
+                runtime_mod.append(gmodel_relaxed.Runtime)
+            except RuntimeError:
+                print ("unable to evaluate true matchin perf of gurobi model in "+fname)
+        except (OSError, NameError, ValueError,RuntimeError):
+            print "unable to process gurobi model in "+fname
+
+
+
+np.savetxt("runtime.csv", runtime_mod, delimiter=",")
+np.savetxt("tm.csv", runtime_mod, delimiter=",")
+
+tm_df = pd_df({
+    "tm": truematch_orig + truematch_mod,
+    "runtime": runtime_orig + runtime_mod,
+    "orig_or_mod": ["mod"]*len(runtime_mod) # ["orig"]*nmc
+})
+nmc = len(runtime_mod)
+
+
+
+
+#print gg.ggplot(tm_df, aes('orig_or_mod', 'runtime')) + \
+#  gg.geom_line(colour='steelblue')
+
+comp_plot = gg.ggplot(data=tm_df, aesthetics=gg.aes(x='runtime', y='tm')) + gg.geom_point() + gg.scale_x_log10()
+gg.ggsave("graphmatch_IP_runtime_vs_tm.pdf",plot = comp_plot)
+print   "mean of tm:"+ str(np.mean( truematch_mod))
--- GraphMatching_Infeasible_compare.py
+++ GraphMatching_Infeasible_compare.py
@@ -25,12 +25,12 @@ root_dir = os.path.join(pr_dir, 'SeededGraphMatch')
 
 
 
-n = 35
+n = 45
 m = 10
 
 mplusn = m+n
 p = 0.2
-corr = 0.7
+corr = 0.6
 nmc = 20
 
 
@@ -76,12 +76,12 @@ runtime_orig = []
 truematch_mod = []
 runtime_mod = []
 
+mc = 0
+mc_npy = np.array([mc])
+session.putvalue('tag', mc_npy)
 
 for mc in range(nmc):
-
     run_script = """run ./src/GM_exact_model_general_param.m"""
-    #mc_npy = np.array([mc])
-    #session.putvalue('tag', mc_npy)
     set_tag =  'tag=uint32('+str(mc)+')'
     print set_tag
     session.run(set_tag)
@@ -151,177 +151,11 @@ tm_df = pd_df({
     "orig_or_mod": ["orig"]*nmc + ["mod"]*nmc
 })
 
-p_val_truematch_diff = st.ttest_ind(truematch_orig, truematch_mod)
-p_val_timediff = st.ttest_ind(runtime_orig, runtime_mod)
+p_val_truematch_diff = st.ttest_rel(truematch_orig, truematch_mod)
+p_val_timediff = st.ttest_rel(runtime_orig, runtime_mod)
 
-
-#print gg.ggplot(tm_df, aes('orig_or_mod', 'tm')) + \
-#  gg.geom_line(colour='steelblue')
-
-
-#print gg.ggplot(tm_df, aes('orig_or_mod', 'runtime')) + \
-#  gg.geom_line(colour='steelblue')
-
-
-
-
-
-
-
-#!/usr/bin/python
-
-import sys
-
-__author__ = 'Sancar'
-
-from gurobipy import *
-import os
-import copy
-import pymatlab
-import numpy as np
-import ggplot as gg
-from scipy import stats as st
-
-from pandas import DataFrame as pd_df
-
-pr_dir = os.environ.get('PROJECT_DIR')
-session = pymatlab.session_factory()
-
-
-#pr_dir = '~/projects/'
-print pr_dir
-
-root_dir = os.path.join(pr_dir, 'SeededGraphMatch')
-
-
-
-n = 35
-m = 10
-
-mplusn = m+n
-p = 0.2
-corr = 0.7
-nmc = 20
-
-
-m_npy = np.array([m])
-
-n_npy = np.array([n])
-mplusn_npy = np.array([mplusn])
-p_npy = np.array([p])
-corr_npy = np.array([corr])
-
-
-
-session.putvalue('m', m_npy)
-session.putvalue('n', n_npy)
-session.putvalue('mplusn', mplusn_npy)
-session.putvalue('p', p_npy)
-session.putvalue('corr', corr_npy)
-
-
-convert_to_dbl = [''.join([x, "=double(", x, ")"]) for x in ["m", "n", "mplusn", "p", "corr"]]
-
-
-for scr in convert_to_dbl:
-    print scr
-    session.run(scr)
-
-chdir_script = """cd """ + root_dir
-#chdir_script = """cd G:\\Sancar\\Documents\\projects\\SeededGraphMatch"""
-session.putvalue('chdir_script', chdir_script)
-
-
-session.run('eval(chdir_script)')
-session.run('cur_dir  = pwd')
-returndir = session.getvalue('cur_dir')
-print returndir
-session.run('addpath(genpath(pwd))')
-session.run("addpath('lib')")
-session.run("addpath('src')")
-
-
-truematch_orig = []
-runtime_orig = []
-truematch_mod = []
-runtime_mod = []
-
-run_script = """run ./src/GM_exact_model_general_param.m"""
-for mc in range(nmc):
-    #mc_npy = np.array([mc])
-    #session.putvalue('tag', mc_npy)
-    set_tag ='tag='+str(mc)
-    print set_tag
-    session.run(set_tag)
-    print session.getvalue('tag')
-    session.putvalue('run_script', run_script)
-    session.run('eval(run_script)')
-    orig_model_fname = session.getvalue('fname_1')
-    infeas_model_fname = session.getvalue('fname_2')
-    gmodel = read(os.path.join(returndir, 'src', infeas_model_fname))
-    gmodel.update()
-    allConstr = gmodel.getConstrs()
-    relaxedConstr_22= allConstr[0:(n*n)]
-    relaxedConstr_12= allConstr[(n*n):(n*n+2*m*n)]
-    pen_Constr_22 = [1.0]*len(relaxedConstr_22)
-    pen_Constr_12 = [1.0]*len(relaxedConstr_12)
-    gmodel_relaxed = gmodel.copy()
-    gmodel_relaxed.feasRelax(0, False, None, None, None, relaxedConstr_22+relaxedConstr_12, \
-                             pen_Constr_22+pen_Constr_12)
-    gmodel_relaxed.update()
-    gmodel_relaxed.write(infeas_model_fname + 'relaxed.lp')
-    optresults = gmodel_relaxed.optimize()
-    v = gmodel_relaxed.getVars()
-    P = np.zeros((n*n, 1))
-    it = 0
-    for var in v[0:(n*n)]:
-        #print var.VarName, var.X
-        P[it, 0] = var.X
-        it += 1
-    P = P.reshape((n, n)).transpose()
-    session.putvalue('Phat', P)
-    #run_script = "Psoln = DS2Perm(Phat)"
-    #session.run(run_script)
-    run_script = '[tm_m'+str(mc) +', delta_m'+str(mc) +'] = PermMat2TrueMatch(Phat, true_perm, m)'
-    session.run(run_script)
-    truematch_orig.append(session.getvalue("delta_m"+str(mc)))
-    runtime_orig.append(gmodel_relaxed.Runtime)
-    pen_Constr_12 = [5.0]*len(relaxedConstr_12)
-    gmodel_relaxed_alt = read(os.path.join(returndir, 'src', infeas_model_fname))
-    gmodel_relaxed_alt.feasRelax(0, False, None, None, None, relaxedConstr_22+relaxedConstr_12, \
-                                 pen_Constr_22+pen_Constr_12)
-    gmodel_relaxed_alt.update()
-    optresults = gmodel_relaxed_alt.optimize()
-    v_alt = gmodel_relaxed_alt.getVars()
-    P_alt = np.zeros((n*n, 1))
-    it = 0
-    for var_a in v_alt[0:(n*n)]:
-        #print var.VarName, var.X
-        P_alt[it, 0] = var_a.X
-        it += 1
-    P_alt = P_alt.reshape((n, n)).transpose()
-    session.putvalue('mod_Phat', P_alt)
-    perm_equal = np.allclose(P, P_alt)
-    print "Two permutation solutions are same? "+str(perm_equal)
-    print "Nonzero elements are "+str(np.nonzero(P-P_alt))
-    run_script = '[mod_tm_m'+str(mc) +', mod_delta_m'+str(mc) +'] = PermMat2TrueMatch(mod_Phat, true_perm, m)'
-    session.run(run_script)
-    truematch_mod.append(session.getvalue("mod_delta_m"+str(mc)))
-    runtime_mod.append(gmodel_relaxed_alt.Runtime)
-    #tuning = gmodel_relaxed.tune()
-    #gmodel_relaxed.getTuneresult(0)
-    #gmodel_relaxed.optimize()
-    # optimal_runtime[mc] =  gmodel_relaxed.Runtime
-
-tm_df = pd_df({
-    "tm": truematch_orig + truematch_mod,
-    "runtime": runtime_orig + runtime_mod,
-    "orig_or_mod": ["orig"]*nmc + ["mod"]*nmc
-})
-
-p_val_truematch_diff = st.ttest_ind(truematch_orig, truematch_mod)
-p_val_timediff = st.ttest_ind(runtime_orig, runtime_mod)
-
+p_val_truematch_diff = st.wilcoxon(truematch_orig, truematch_mod)
+p_val_timediff = st.wilcoxon(runtime_orig, runtime_mod)
 
 #print gg.ggplot(tm_df, aes('orig_or_mod', 'tm')) + \
 #  gg.geom_line(colour='steelblue')
@@ -329,6 +163,14 @@ p_val_timediff = st.ttest_ind(runtime_orig, runtime_mod)
 
 #print gg.ggplot(tm_df, aes('orig_or_mod', 'runtime')) + \
 #  gg.geom_line(colour='steelblue')
+
+comp_plot = gg.ggplot(data=tm_df, aesthetics=gg.aes(x='runtime', y='tm', colour='orig_or_mod')) + gg.geom_point() + gg.scale_x_log10()
+gg.ggsave("orig_IP_vs_modified_IP_3.pdf",plot = comp_plot)
+
+
+
+
+
 
 
 
